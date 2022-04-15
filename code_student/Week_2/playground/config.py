2ab1bd9c64a00fbdd3aa05859daaa2dba97d89c7
FourthBrain <72572922+FourthBrain@users.noreply.github.com>
2022-04-15 10:41:41 -0500

    gpt resources

---
+++ code_student/Week_2/playground/config.py
@@ -0,0 +1,8 @@
+HF_TOKENIZER_NEWS_CLASSIFIER = "mrm8488/bert-mini-finetuned-age_news-classification"
+HF_MODEL_NEWS_CLASSIFIER = "mrm8488/bert-mini-finetuned-age_news-classification"
+
+SUBREDDITS = "wallstreetbets+investing+options+pennystocks+options+stocks"
+
+NUM_SUBMISSION_TO_GET = 1000
+
+NEWS_CLASSES = ["world", "sports", "business", "sci/tech"]
